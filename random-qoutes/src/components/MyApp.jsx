2aa4f6d064fc3487ce497147e72cfb0a1655da98
Rohit <stylishrb711@gmail.com>
2022-05-19 14:27:11 +0530

    done

--- random-qoutes/src/components/MyApp.jsx
+++ random-qoutes/src/components/MyApp.jsx
@@ -3,24 +3,25 @@ import axios from "axios";
 import "../App.css";
 
 const MyApp = () => {
-  const [newAdvice, setNewAdvice] = useState("Hello");
+  const [newAdvice, setNewAdvice] = useState("");
 
-  useEffect(() => {
+  function generateQoute() {
     axios
       .get("https://api.adviceslip.com/advice")
       .then((res) => {
         var advice = res.data.slip.advice;
-        console.log(advice);
+        setNewAdvice(advice);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }
 
   return (
     <>
       <h1>Random qoutes generator</h1>
       <h2>{newAdvice}</h2>
+      <button onClick={generateQoute}>Click</button>
     </>
   );
 };
