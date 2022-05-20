db99efd8b49583615aa3003defab2ad95d783c37
Rohit <stylishrb711@gmail.com>
2022-05-20 07:23:35 +0530

    ocmponents created

--- newsApp/src/Components/NewsItem.jsx
+++ newsApp/src/Components/NewsItem.jsx
@@ -2,7 +2,23 @@ const NewsItem = () => {
   return (
     <>
       <div className="news-item-div">
-        <h1>Breaking News</h1>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            className="card-img-top"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Square_200x200.svg/768px-Square_200x200.svg.png"
+            alt="Card image cap"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
       </div>
     </>
   );
