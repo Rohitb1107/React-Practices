57e7982d20c6b8de145f25e08174dc7e34dc6a45
Rohit <stylishrb711@gmail.com>
2022-05-20 09:24:04 +0530

    css update

--- newsApp/src/Components/News.jsx
+++ newsApp/src/Components/News.jsx
@@ -4,8 +4,42 @@ import NewsItem from "./NewsItem";
 const News = () => {
   return (
     <>
-      <div className="news-div">
-        <NewsItem />
+      <div className="news-div container my-3">
+        <div className="row pb-3">
+          <div className="col-md-4">
+            <NewsItem />
+          </div>
+          <div className="col-md-4">
+            <NewsItem />
+          </div>
+          <div className="col-md-4">
+            <NewsItem />
+          </div>
+        </div>
+
+        <div className="row pb-3">
+          <div className="col-md-4">
+            <NewsItem />
+          </div>
+          <div className="col-md-4">
+            <NewsItem />
+          </div>
+          <div className="col-md-4">
+            <NewsItem />
+          </div>
+        </div>
+
+        <div className="row pb-3">
+          <div className="col-md-4">
+            <NewsItem />
+          </div>
+          <div className="col-md-4">
+            <NewsItem />
+          </div>
+          <div className="col-md-4">
+            <NewsItem />
+          </div>
+        </div>
       </div>
     </>
   );
