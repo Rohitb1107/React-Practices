6bee3c22a0bd288fe85a92e14328a5171cda0022
Rohit <stylishrb711@gmail.com>
2022-06-02 23:33:22 +0530

    header added

--- learn-context-2/src/App.jsx
+++ learn-context-2/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <h1>Learn Context API</h1>
+      <Header />
     </div>
   );
 }
