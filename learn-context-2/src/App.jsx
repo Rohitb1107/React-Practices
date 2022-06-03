1f016e81922814f6bac9f600426e87d90f808f14
Rohit <stylishrb711@gmail.com>
2022-06-03 14:17:07 +0530

    routes added

--- learn-context-2/src/App.jsx
+++ learn-context-2/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <div className="App">
-      <h1>Learn Context API</h1>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+      </Routes>
     </div>
   );
 }
--- learn-context-2/src/components/Header.jsx
+++ learn-context-2/src/components/Header.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <h1>Learn Context API</h1>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/cart">Cart</Link> </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
